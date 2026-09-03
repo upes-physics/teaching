c37e4265e034a8e1f6fc826846782f857b02815d
sroychow <suvankar.roy.chowdhury@cern.ch>
2026-09-03 07:10:50 +0000

    Deploying to gh-pages from @ upes-physics/teaching@14056509048f23c9a97efaa90ccfc5db6edb9b0c 🚀

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const catalog = {
 	    ],
 	    5: [
 		{id : "qmbsc", code: "PHYS3058", name: "Quantum Mechanics and Application", description: "", dataCard: "data/ug/sem5/phys3058/datacard.json"},
-		{id : "qmbsc", code: "PHYS3059", name: "Atomic and Molecular Physics", description: "", dataCard: "data/ug/sem5/phys3059/datacard.json"}
+		{id : "atomicbsc", code: "PHYS3059", name: "Atomic and Molecular Physics", description: "", dataCard: "data/ug/sem5/phys3059/datacard.json"}
 	    ],
 	    6: [],
 	    7: [
